3c80d46677dbb8dbf3ffcaef57ab94306a8b9b1a
DanielCarey04 <87291681+DanielCarey04@users.noreply.github.com>
2021-07-11 22:27:16 -0500

    Add files via upload

---
+++ application/views/login.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <div class="container">
+    <header class="d-flex justify-content-center  py-3 mb-4 border-bottom">
+    <h1 class="mb-3 fw-normal">Admin Login</h1>
+    </header>
+  </div>
+
+  <link href="application\assets\login.css" rel="stylesheet">
+    </head>
+  <body class="text-center">
+  <div class="container">
+<main class="form-login">
+  <form>
+    <h1 class="h3 mb-3 fw-normal">LOGIN</h1>
+    <div class="container">
+    <div class="form-floating mb-3">
+      <input type="Usuario" class="form-control" id="floatingInput" placeholder="Usuario">
+      <label for="floatingInput">Usuario</label>
+    </div>
+</div> 
+<div class="container-sm">
+    <div class="form-floating mb-3">
+      <input type="contraseña" class="form-control" id="floatingPassword" placeholder="Contraseña">
+      <label for="floatingPassword">Contraseña</label>
+    </div>
+</div>
+    <button class="w-100 btn btn-lg btn btn-primary" type="submit">Entrar</button>
+    </form>
+</main>
+</div>
+
+    
+  </body>
+</html>
